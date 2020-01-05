bf8088792d6d5c3e0e689652d6e3e82a602de2d9
Hayley Walters <hwalters.alt@gmail.com>
2020-01-05 14:07:36 -0800

    edited image editing into main(). edited how noaa information is handled

--- Stevens Pass Weather.py
+++ Stevens Pass Weather.py
@@ -8,9 +8,12 @@ Created on Fri Dec 27 13:05:54 2019
 
 
 """
+CHANGE_WALLPAPER = True
+
 import requests
 from bs4 import BeautifulSoup
-
+import os
+import re
 
 def get_page_content(url):
     agent = {"User-Agent":'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36'}
@@ -33,6 +36,8 @@ def remove_letters(string):
 
 #items with the same key, values combined into tuple. also contains a 
 def combine_two_dicts(dict1, dict2):
+    days = ["Monday", "Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+    
     if len(dict1) > len(dict2):
         bigger = dict1
         smaller = dict2
@@ -50,8 +55,14 @@ def combine_two_dicts(dict1, dict2):
     smaller_val_list = list(smaller.values())
     
     final_dict = dict()
-    for i in range(0,len(bigger)):
-        if (bigger_key_list[i] in smaller_key_list) or (bigger_key_list[i] == "Today" and smaller_key_list[i] == "Tonight"):
+    start = 0
+    if bigger_key_list[0] not in days and smaller_key_list[0] not in days:
+            final_dict["Today"] = (bigger_val_list[0], smaller_val_list[0])
+            start = 1
+            
+    for i in range(start,len(bigger)):
+        
+        if (bigger_key_list[i] in smaller_key_list):
             #find the index of the key in dict 2 for the value
             smaller_key_index = smaller_key_list.index(bigger_key_list[i])
             new_val = (bigger_val_list[i],smaller_val_list[smaller_key_index])
@@ -68,6 +79,7 @@ def combine_two_dicts(dict1, dict2):
 
 def main():
     #NOAA HIGHS AND LOWS + SHORT DESCRIPTION
+    
     noaa_soup = get_page_content("https://forecast.weather.gov/MapClick.php?lat=47.75&lon=-121.09#.XgsYhRdKhQI")
     noaa_results = noaa_soup.find(id='seven-day-forecast-list')
     
@@ -78,34 +90,36 @@ def main():
     noaa_desc = dict()
     
     for forecast in noaa_twelve_hr_forecasts:
-        if forecast.find('p', class_='temp-low') == None:
+        if forecast.find('p', class_='temp-low') == None and forecast.find('p', class_="temp-high") != None:
             day_title = str(forecast.find('p', class_='period-name').text)
+            day_title = re.sub(r"(?<=\w)([A-Z])", r" \1", day_title)
             #removes all tags and then removes all whitespace
-            day_title = day_title.replace(" ","")
-            
             high_temp = str(forecast.find('p', class_='temp-high').text)
             #removes all letters and all html tags from high_temp
             high_temp = "High "+remove_letters(high_temp)
             
             noaa_high_temps[day_title] = high_temp
             
-        if forecast.find('p', class_='temp-high') == None:
+            description = str(forecast.find('p', class_='short-desc').text)
+            noaa_desc[day_title] = description
+            
+        elif forecast.find('p', class_='temp-high') == None and forecast.find('p', class_='temp-low') != None:
             day_title= str(forecast.find('p', class_='period-name').text)
-            day_title = day_title.replace("Night","").replace(" ","")
+            day_title = day_title.replace("Night","")
             
             low_temp = str(forecast.find('p', class_='temp-low').text)
             #removes all letters and all html tags
             low_temp = "Low "+remove_letters(low_temp)
             
             noaa_low_temps[day_title] = low_temp
-        
-        description = str(forecast.find('p', class_='short-desc').text)
-        noaa_desc[day_title] = description
+            
+            description = str(forecast.find('p', class_='short-desc').text)
+            noaa_desc[day_title] = description
     
-    noaa_all_temp = combine_two_dicts(noaa_high_temps, noaa_low_temps)
+    noaa_all_temps = combine_two_dicts(noaa_high_temps, noaa_low_temps)
     
     print("~~~~0~~~~\nWeather Forecast NOAA:\n")
-    print_dict(noaa_all_temp)
+    print_dict(noaa_all_temps)
     print("~~~~\nWeather Descriptions NOAA:\n")
     print_dict(noaa_desc)
     
@@ -194,6 +208,35 @@ def main():
     print_dict(weather_desc)
     print("~~~~\nWeather Precip Chances Weather Channel:\n")
     print_dict(weather_precip_chances)
+    
+    #EDIT THE TEMPLATE INFOGRAPHIC
+    os.remove("Stevens Pass Infographic Template.jpg")
+    print("Copy template removed successfully")
+    from PIL import Image, ImageDraw, ImageFont
+
+    im = Image.open('Stevens Pass Infographic.jpg')
+    font = ImageFont.truetype('arial.ttf', size=20)
+    color = (105,105,105)
+    
+    # initialise the drawing context with the image object as background
+    draw = ImageDraw.Draw(im)
+    
+    #DISPLAY NOAA DATA ONTO JPG
+    noaa_day_titles = list(noaa_all_temps.keys())
+    accu_day_titles = list(accu_all_temps.keys())
+    weather_day_titles = list(weather_all_temps.keys())
+    
+    
+    for i in range(3):
+        break
+    
+    draw.text((50,150), noaa_day_titles[0], fill=color, font=font)
+        
+        
+    im.show()
+    image_name_output = 'Stevens Pass Infographic Template.jpg'
+    im.save(image_name_output)
+    
     
 if __name__ == "__main__":
     main()
